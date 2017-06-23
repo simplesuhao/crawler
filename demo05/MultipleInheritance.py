55e32cee0838bcfe16dd88e1a7dcbbd2919099a1
su_xiao <simplesuhao@163.com>
2017-06-23 17:39:56 +0800

    MultipleInteritanceDemo

---
+++ demo05/MultipleInheritance.py
@@ -0,0 +1,29 @@
+class Animal(object):
+    pass
+
+class Mammal(Animal):
+    pass
+
+class Bird(Animal):
+    pass
+
+class Dog(Mammal):
+    pass
+
+class Bat(Mammal):
+    pass
+
+class Parrot(Bird):
+    pass
+
+class Ostrich(Bird):
+    pass
+
+class Runnable(object):
+    def run(self):
+        print('running')
+
+class Flyable(object):
+    def fly(self):
+        print('flying')
+
