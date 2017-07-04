7362266c66a71d4172a10921d4ff50491d9df677
蘇子耀 <simplesuhao@163.com>
2017-07-04 22:38:26 +0800

    typeDemo

---
+++ demo07/ListMetaclass.py
@@ -0,0 +1,7 @@
+class ListMetaclass(type):
+    def __new__(cls, name, bases, attrs):
+        attrs['add'] = lambda self, value: self.append(value)
+        return type.__new__(cls, name, bases, attrs)
+
+class MyList(list):
+    __metaclass__ = ListMetaclass
